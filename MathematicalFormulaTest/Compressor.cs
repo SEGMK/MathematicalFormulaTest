7f7fa8bc451935bf52f121b29338ace4e1f5b13a
SEGMK <segmk.wilder@gmail.com>
2022-10-10 21:13:48 +0200

    Dodaj pliki projektów.

---
+++ MathematicalFormulaTest/Compressor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicalFormulaTest
+{
+    static class Compressor
+    {
+        public static (int, ulong, List<ulong>) CompressByteArray(ulong originalByteArray)
+        {
+            ulong byteArray = originalByteArray;
+            List<ulong> restFromSqrtEnumerable = new List<ulong>();
+            int powerOfTheCompression = 0;
+            do
+            {
+                //zmiana reszty z Sqrt(byteArray) na reprezentacje tej reszty w byteArray i zapisanie roznicy ByteArray i reprezentacji reszty w strukturze danych
+                ulong resoult = (ulong)Math.Sqrt(byteArray);
+                ulong test = resoult * resoult;
+                ulong restFromSqrt = byteArray - (resoult * resoult);
+                restFromSqrtEnumerable.Add(restFromSqrt);
+                //odjecie reszty i kompresja
+                byteArray -= restFromSqrt;
+                //zapisanie liczby do daleszej kompresacji
+                byteArray = (ulong)Math.Sqrt(byteArray);
+                //zapisanie potegi kopresjii
+                powerOfTheCompression += 2;
+
+            } while (100 <= byteArray);
+            return (powerOfTheCompression, byteArray, restFromSqrtEnumerable);
+        }
+        public static ulong DecompressByteArray(int compressionPower, ulong compressedByteArray, List<ulong> restFromCompression)
+        {
+            restFromCompression.Reverse();
+            for (int i = 0; i < restFromCompression.Count; i++)
+            {
+                compressedByteArray = compressedByteArray * compressedByteArray;
+                compressedByteArray += restFromCompression[i];
+            }
+            return compressedByteArray;
+        }
+    }
+}
---
+++ MathematicalFormulaTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MathematicalFormulaTest
+{
+    class Program
+    {
+        //2 147 483 647
+        //13 950 991 169 621 486 829
+        // sqrt 3 735 102 564
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wprowadź kompresowaną wartość");
+            ulong OriginalByteArray = ulong.Parse(Console.ReadLine());
+            (int, ulong, List<ulong>) compressedFile = Compressor.CompressByteArray(OriginalByteArray);
+            ulong decompressedByteArray;
+            decompressedByteArray = Compressor.DecompressByteArray(compressedFile.Item1, compressedFile.Item2, compressedFile.Item3);
+            Console.WriteLine($"Sucessfull compression and decompression: {decompressedByteArray == OriginalByteArray}");
+            Console.WriteLine($"Original value: \t {OriginalByteArray}");
+            Console.WriteLine($"Decopressed value: \t {decompressedByteArray}");
+            Console.WriteLine($"Compressed value: \t {compressedFile.Item2}");
+        }
+    }
+}
