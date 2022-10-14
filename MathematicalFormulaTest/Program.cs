e9083e297d172dd86b75a5ef59d31850a6e24a6f
SEGMK <85564914+SEGMK@users.noreply.github.com>
2022-10-14 15:39:37 +0200

    CompressFileByDividing
    Creation of CompressFileByDividing() and made few changes in displaying the resoults of the compression

--- MathematicalFormulaTest/Program.cs
+++ MathematicalFormulaTest/Program.cs
@@ -14,20 +14,32 @@ namespace MathematicalFormulaTest
         {
             Console.WriteLine("Wprowadź kompresowaną wartość");
             OriginalByteArray = ulong.Parse(Console.ReadLine());
-            CompressFileBySqrt();
+            //CompressFileBySqrt();
+            CompressFileByDividing();
+        }
+        public static void CompressFileByDividing()
+        { 
+            (byte, List<byte>, Dictionary<byte, byte>) compressedFile = 
+                CompressorDivided.CompressByteArray(OriginalByteArray);
+            DecompressedByteArray = CompressorDivided.DecompressByteArray(compressedFile.Item1,
+                compressedFile.Item2, compressedFile.Item3);
+            
+            PrintOutCompressionResoult(compressedFile.Item1, "CompressorDivided");
         }
         public static void CompressFileBySqrt()
         {
             (int, ulong, List<ulong>) compressedFile = CompressorSqrt.CompressByteArray(OriginalByteArray);
             DecompressedByteArray = CompressorSqrt.DecompressByteArray(compressedFile.Item1, compressedFile.Item2, compressedFile.Item3);
-            PrintOutCompressionResoult((byte)compressedFile.Item1);
+            PrintOutCompressionResoult((byte)compressedFile.Item1, "CompressorSqrt");
         }
-        public static void PrintOutCompressionResoult(byte compressedFile)
+        public static void PrintOutCompressionResoult(byte compressedFile, string compressionMethod)
         {
+            Console.WriteLine(compressionMethod);
             Console.WriteLine($"Sucessfull compression and decompression: {DecompressedByteArray == OriginalByteArray}");
             Console.WriteLine($"Original value: \t {OriginalByteArray}");
             Console.WriteLine($"Decopressed value: \t {DecompressedByteArray}");
             Console.WriteLine($"Compressed value: \t {compressedFile}");
+            Console.WriteLine();
         }
     }
 }
