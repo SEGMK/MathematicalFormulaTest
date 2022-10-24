79e589ceb07499be69a1e295020119f320f459bb
SEGMK <segmk.wilder@gmail.com>
2022-10-24 18:07:33 +0200

    Full file compression Test

--- MathematicalFormulaTest_Test/CompressorDividedTest.cs
+++ MathematicalFormulaTest_Test/CompressorDividedTest.cs
@@ -2,6 +2,7 @@ using System;
 using Xunit;
 using System.Reflection;
 using MathematicalFormulaTest;
+using System.Collections.Generic;
 
 namespace MathematicalFormulaTest_Test
 {
@@ -27,6 +28,32 @@ namespace MathematicalFormulaTest_Test
             actual = actual.Replace("0", "");
             expected = expected.Replace("0", "");
             Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void CompressionAndDecompressionTest()
+        {
+            //Arrange
+            short[] tab = new short[] { 9, 223, 37, 20, 36, 84 };
+            string expected = (9223037020036084 / 2).ToString();
+            List<int> restFromDivision = new List<int>();
+            //Act
+            var method = typeof(CompressorDivided).GetMethod("DivideByTwo", BindingFlags.Static | BindingFlags.NonPublic);
+            var field = typeof(CompressorDivided).GetField("CompressingInProgressFile", BindingFlags.Static | BindingFlags.NonPublic);
+            field.SetValue(null, tab);
+            method.Invoke(null, null);
+            Compression(field, method, ref restFromDivision);
+        }
+        private void Compression(FieldInfo field, MethodInfo method, ref List<int> restFromDivision)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                if (((short[])field.GetValue(null))[((short[])field.GetValue(null)).Length] % 2 != 0)
+                {
+                    ((short[])field.GetValue(null))[((short[])field.GetValue(null)).Length] -= 1;
+                    restFromDivision.Add(i);
+                }
+                method.Invoke(null, null);
+            }
         }
     }
 }
